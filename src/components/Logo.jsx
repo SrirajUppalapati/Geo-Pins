041f5fa13bd50a3e35194261e44267fe366af595
Sriraj Uppalapati <srirajuppalapati9@gmail.com>
2024-06-20 12:50:25 -0400

    Second Commit

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,8 +2,18 @@ import styles from "./Logo.module.css";
 import { Link } from "react-router-dom";
 function Logo() {
   return (
-    <Link>
-      <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />
+    <Link
+      style={{
+        display: "flex",
+        alignItems: "center",
+        textDecoration: "none",
+        gap: "1rem",
+      }}
+    >
+      <div>
+        <img src="/icon.png" alt="WorldWise logo" className={styles.logo} />
+      </div>
+      <h1 style={{}}>Geo Pins</h1>
     </Link>
   );
 }
